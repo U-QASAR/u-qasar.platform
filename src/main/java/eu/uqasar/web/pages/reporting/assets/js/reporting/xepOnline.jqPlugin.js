0b8554611c6097671adf543907537a871752d509
Sebastian Scholze <scholze@atb-bremen.de>
2015-11-12 15:45:47 +0100

    add license header

--- src/main/java/eu/uqasar/web/pages/reporting/assets/js/reporting/xepOnline.jqPlugin.js
+++ src/main/java/eu/uqasar/web/pages/reporting/assets/js/reporting/xepOnline.jqPlugin.js
@@ -363,7 +363,28 @@ xepOnline.Formatter = {
 	},
 	xep_chandra_service: 'http://xep.cloudformatter.com/Chandra.svc/genpackage',
 	xep_chandra_service_AS_PDF: 'http://xep.cloudformatter.com/Chandra.svc/genfile',
-	entity_declaration:'<!DOCTYPE div [  <!ENTITY % winansi SYSTEM "http://xep.cloudformatter.com/doc/XSL/winansi.xml">  %winansi;]>',
+	entity_declaration:'<!DOCTYPE div [  <!ENTITY % winansi SYSTEM "http://xep.cloudformatter.com/doc/XSL/winansi.xml">  %winansi;
+
+/*
+ * #%L
+ * U-QASAR
+ * %%
+ * Copyright (C) 2012 - 2015 U-QASAR Consortium
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+>',
 	xsl_stylesheet_declaration: '<?xml-stylesheet type="text/xsl" href="http://xep.cloudformatter.com/Doc/XSL/xeponline-fo-translate-2.xsl"?>',
 	src_type: { xml: 'text/xml'},
 	mime_type: { pdf: 'application/pdf', svg: 'image/svg+xml'},
